9aad95f5e0448040b708b01b252542e4a50f4175
Junior <junior.jr.jl@gmail.com>
2022-09-03 15:53:16 -0300

    melhorando codigo do desafio 15

--- medium/challenge_15.js
+++ medium/challenge_15.js
@@ -1,20 +1,27 @@
-const possibleRomanAlgorism = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-const algorism = [1, 5, 10, 50, 100, 500, 1000]
+const numbersConfig = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
 
 exports.romanNumberToNumber = (romanNumber) => {
     const romanAlgorism = romanNumber.toUpperCase().split('')
-    const romanAlgorismIndexes = []
-    romanAlgorism.forEach(a => romanAlgorismIndexes.push(possibleRomanAlgorism.indexOf(a)));
+    const convertedDigits = romanAlgorism.map(a => numbersConfig[a])
     let convertedNumber = 0
-    while(romanAlgorismIndexes.length !== 0){
-        let currElement = romanAlgorismIndexes[0]
-        if (currElement < romanAlgorismIndexes[1]) {
-            convertedNumber = convertedNumber + (algorism[romanAlgorismIndexes[1]] - algorism[currElement])
-            romanAlgorismIndexes.splice(0, 2)
+    while(convertedDigits.length !== 0){
+        const currElement = convertedDigits[0]
+        const nextElement = convertedDigits[1]
+        if (currElement < nextElement) {
+            convertedNumber = convertedNumber + (nextElement - currElement)
+            convertedDigits.splice(0, 2)
         } else {
-            const currElementOccurrences = romanAlgorismIndexes.filter((a, i) => i < 3 && a === currElement).length
-            convertedNumber = convertedNumber + (algorism[currElement] * currElementOccurrences)
-            romanAlgorismIndexes.splice(0, currElementOccurrences)
+            const currElementOccurrences = convertedDigits.filter((a, i) => i < 3 && a === currElement).length
+            convertedNumber = convertedNumber + (currElement * currElementOccurrences)
+            convertedDigits.splice(0, currElementOccurrences)
         }
     }
     return convertedNumber;
